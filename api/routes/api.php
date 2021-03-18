f288074b53e8d2fe442d01d4d5d99e9203f576cb
Rubin Bajracharya <rubinbajracharya@yahoo.com>
2021-03-18 18:28:42 +0545

    - Added Fractal for response formatting - Got details of user

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Auth\SignInController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'auth'], function () {
     Route::post('signin', SignInController::class);
+    Route::get('me', MeController::class);
 });
